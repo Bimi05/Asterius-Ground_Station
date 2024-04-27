fd6d520763461fa5ce436059c503e3e47009a296
Bimi05 <dim.ziakas07@gmail.com>
2024-04-27 11:29:57 +0300

    add: ground station orders to Master

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <Arduino.h>
-
 #include "receiver.h"
 
-#define SD_SPI_ADDR 5
-#define FREQUENCY 434.2F
+#define SD_CHIP_SELECT 5
+#define FREQUENCY 433.2F
 
 Receiver rv(FREQUENCY);
 
@@ -11,15 +10,23 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  if (!(SD.begin(SD_SPI_ADDR) && rv.init())) {
-    Serial.println("Could not initialise sensors, check wirings and addresses.");
-  }
-  Serial.println("Ready!");
+  SD.begin(SD_CHIP_SELECT);
+
+  const char* init = (rv.init()) ? "Ready!" : "Initialisation failed.";
+  Serial.println(init);
 }
 
 void loop() {
   if (rv.receive()) {
     rv.save(rv.message); //* this is modified by .receive(), so we're safe (probably) :)
     rv.log("Packet received. Fabulous.");
+    rv.log(rv.message);
+  }
+
+  while (Serial.available() != 0) {
+    if (Serial.readString() == "detach") {
+      char order[] = "Asterius:DETACH [G->M]";
+      rv.sendMessage(order);
+    }
   }
 }
