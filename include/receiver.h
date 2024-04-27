70b13e97c6964c71e84cadc050d7d53fe9aa3fdb
Bimi05 <dim.ziakas07@gmail.com>
2024-04-27 11:29:27 +0300

    update: include guard, code rearrangements

--- include/receiver.h
+++ include/receiver.h
@@ -1,37 +1,45 @@
-#pragma once
+#ifndef __RECEIVER__
+#define __RECEIVER__
 
 #include <SD.h>
 #include <RH_RF95.h>
 
 class Receiver {
   private:
-    File df; // our data file
-    float freq;
-    uint32_t lastKnownM_PID;
-    uint32_t lastKnownS_PID;
+    uint32_t lastM_PID;
+    uint32_t lastS_PID;
 
-    // "Fills" in blank packets in a range of IDs.
-    void fill(uint32_t start, uint32_t end, char unit);
+    File df;
+    float freq;
+
+    // Fills in blank packets in a range of IDs
+    void fill(uint32_t, uint32_t, char);
 
     // Decrypt the incoming data
-    void process(uint8_t mode, char* data, uint8_t offset);
+    char* process(uint8_t, char*, uint8_t);
 
   public:
-    char* message;
-
-    Receiver(float frequency);
+    Receiver(float);
     ~Receiver();
 
+    char* message;
+
     // Initialise and configure the receiver (using the RH_RF95 module)
+    // Return true if initialisation was successful
     bool init();
 
     // Check if we were able to receive a packet
-    // Return true if yes, false otherwise
+    // Return true if a packet was received
     bool receive();
 
     // Saves most recent stored message to the micro-SD card
-    void save(char* message);
+    void save(char*);
 
     // Log a message to the serial monitor
-    void log(const char* message);
+    void log(const char*);
+
+    // Send a message to the Master CanSat
+    void sendMessage(char*);
 };
+
+#endif
